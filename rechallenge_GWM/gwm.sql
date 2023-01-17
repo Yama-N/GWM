b4288dc29506464de813b38ed29a6ecfa968aa39
AzureAD\ジョブトレIT･WEB渋谷　山下奈緒美 <naomi.yamashita_04@atgp-jobtra.jp>
2023-01-17 10:29:52 +0900

    .sql更新

--- rechallenge_GWM/gwm.sql
+++ rechallenge_GWM/gwm.sql
@@ -1,5 +1,5 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 2.11.11.3
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
@@ -7,15 +7,13 @@
 -- サーバのバージョン： 10.4.24-MariaDB
 -- PHP のバージョン: 8.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+--
+-- データベース: `fitsuki_gwm`
+--
+CREATE DATABASE `fitsuki_gwm` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `fitsuki_gwm`;
 
 --
 -- データベース: `gwm`
@@ -27,33 +25,36 @@ SET time_zone = "+00:00";
 -- テーブルの構造 `data`
 --
 
-CREATE TABLE `data` (
+CREATE TABLE IF NOT EXISTS `data` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `start_hour` int(2) DEFAULT NULL,
-  `start_min` int(2) DEFAULT NULL,
-  `end_hour` int(2) DEFAULT NULL,
-  `end_min` int(2) DEFAULT NULL,
-  `is_bounce` int(2) DEFAULT NULL,
-  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `start_hour` int(2) default NULL,
+  `start_min` int(2) default NULL,
+  `end_hour` int(2) default NULL,
+  `end_min` int(2) default NULL,
+  `is_bounce` int(2) default NULL,
+  `comment` varchar(255) character set utf8 collate utf8_unicode_ci default NULL,
+  `updated_at` datetime NOT NULL,
+  `login_flag` int(1) NOT NULL default '0',
+  KEY `login_flag` (`login_flag`),
+  FULLTEXT KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `data`
 --
 
-INSERT INTO `data` (`id`, `name`, `start_hour`, `start_min`, `end_hour`, `end_min`, `is_bounce`, `comment`, `updated_at`) VALUES
-(1, '木下 吉彦', 2, 30, 2, 30, 0, 'kkkk', '2022-11-16 15:28:40'),
-(2, '遠藤 孝彦', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:50:21'),
-(3, '沖中 宏之', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:23:48'),
-(4, '澤村 駿', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:17'),
-(5, '土橋 高明', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:34'),
-(6, '鶴岡 裕樹', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:36'),
-(7, '佐々木 千鶴', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:37'),
-(8, '中山 奈都美', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:38'),
-(9, '渡辺 亜美', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:39'),
-(10, '大森 満', NULL, NULL, NULL, NULL, 0, '', '2022-11-16 14:25:40');
+INSERT INTO `data` (`id`, `name`, `start_hour`, `start_min`, `end_hour`, `end_min`, `is_bounce`, `comment`, `updated_at`, `login_flag`) VALUES
+(2, '遠藤 孝彦', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(3, '沖中 宏之', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(4, '澤村 駿', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(5, '土橋 高明', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(7, '佐々木 千鶴', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(6, '鶴岡 裕樹', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(8, '中山 奈都美', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(9, '渡辺 亜美', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(10, '大森 満', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0),
+(1, '木下 吉彦', NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', 0);
 
 --
 -- ダンプしたテーブルのインデックス
